cc06ef9f06233e5db15e97f5f0610125960b9598
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-05-24 14:31:28 +0900

    [Add]Users Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'user/new'
+# ルートページを設定
   root 'homes#top'
-  get 'homes/about'
+# この書き方でURLを指定する
+  get '/about', to: "homes#about"
+# ユーザー登録用
+  get '/signup', to: "users#new"
+# RESTfulなUsersリソースで必要となるすべてのアクションが利用できるようになる
+  resources:users
 end
